3f642e45992435f82d521ad287c671447f70007a
vitor-valentin <vitor.rohling.becker@gmail.com>
2025-05-20 16:02:48 -0300

    - Initial commit - Main structure

---
+++ script.js
@@ -0,0 +1,45 @@
+const sidebar = document.getElementById("sidebar-placeholder");
+const header = document.getElementById("header-placeholder");
+const page = document.getElementById("page-placeholder");
+
+fetch('templates/header.html')
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Erro ao carregar a sidebar');
+        }
+        return response.text();
+    })
+    .then(data => {
+        header.innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+});
+
+fetch('pages/servicos.html')
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Erro ao carregar a sidebar');
+        }
+        return response.text();
+    })
+    .then(data => {
+        page.innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+});
+
+fetch('templates/sidebar.html')
+    .then(response => {
+        if (!response.ok) {
+        throw new Error('Erro ao carregar a sidebar');
+        }
+        return response.text();
+    })
+    .then(data => {
+        sidebar.innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+});
