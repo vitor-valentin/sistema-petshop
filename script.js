94ceb527e09b63b92afdbd7b1da22950ce97159b
vitor-valentin <vitor.rohling.becker@gmail.com>
2025-05-21 19:43:35 -0300

    - Adding files to pages for previous structure creation - Changed clientes.html structure of using two content classes - Removed status from clientes table - Sidebar buttons working, changing pages, animation - Sidebar close button working

--- script.js
+++ script.js
@@ -1,45 +1,99 @@
-const sidebar = document.getElementById("sidebar-placeholder");
-const header = document.getElementById("header-placeholder");
-const page = document.getElementById("page-placeholder");
+const sidebarPlaceholder = document.getElementById("sidebar-placeholder");
+const headerPlaceholder = document.getElementById("header-placeholder");
+const pagePlaceholder = document.getElementById("page-placeholder");
 
-fetch('templates/header.html')
-    .then(response => {
-        if (!response.ok) {
-        throw new Error('Erro ao carregar a sidebar');
-        }
-        return response.text();
-    })
-    .then(data => {
-        header.innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Erro:', error);
+Promise.all([
+    fetch("templates/header.html")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Erro ao carregar o cabeçalho");
+            }
+            return response.text();
+        })
+        .then((data) => {
+            headerPlaceholder.innerHTML = data;
+        }),
+
+    fetch("pages/clientes.html")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Erro ao carregar a página clientes");
+            }
+            return response.text();
+        })
+        .then((data) => {
+            pagePlaceholder.innerHTML = data;
+        }),
+
+    fetch("templates/sidebar.html")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Erro ao carregar a sidebar");
+            }
+            return response.text();
+        })
+        .then((data) => {
+            sidebarPlaceholder.innerHTML = data;
+        }),
+])
+.then(() => {
+    initializeCode();
+})
+.catch((error) => {
+    console.error("Erro:", error);
 });
 
-fetch('pages/clientes.html')
-    .then(response => {
-        if (!response.ok) {
-        throw new Error('Erro ao carregar a sidebar');
-        }
-        return response.text();
-    })
-    .then(data => {
-        page.innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Erro:', error);
-});
+function initializeCode() {
+    const sidebarBtn = document.querySelector(".menu");
+    const headerMenu = document.querySelector("header");
+    const sidebar = document.querySelector(".sidebar");
+    const sidebarBtns = document.querySelectorAll("#sidebarBtn");
 
-fetch('templates/sidebar.html')
-    .then(response => {
-        if (!response.ok) {
-        throw new Error('Erro ao carregar a sidebar');
-        }
-        return response.text();
-    })
-    .then(data => {
-        sidebar.innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Erro:', error);
-});
+    var page = document.querySelector(".content");
+    
+
+    function toggleSideBar() {
+        sidebar.classList.toggle("hidden");
+        headerMenu.classList.toggle("full");
+        page.classList.toggle("full");
+    }
+
+    function changePage(redPage) {
+        fetch("pages/"+ redPage +".html")
+            .then((response) => {
+                if(!response.ok)
+                {
+                    throw new Error("Error ao carregar a página " + redPage);
+                }
+                return response.text();
+            })
+            .then((data) => {
+                pagePlaceholder.innerHTML = data;
+                page = document.querySelector(".content");
+            });
+    }
+
+    function removeCurrActiveBtnClass()
+    {
+        sidebarBtns.forEach((btn) => {
+            if(btn.classList.contains("active"))
+            {
+                btn.classList.remove("active");
+            }
+        });
+    }
+
+    sidebarBtn.addEventListener("click", () => {
+        toggleSideBar();
+    });
+
+    sidebarBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            let redirectPage = btn.getAttribute("redirectPage");
+            changePage(redirectPage, btn);
+
+            removeCurrActiveBtnClass();
+            btn.classList.add("active");
+        });
+    });
+}
